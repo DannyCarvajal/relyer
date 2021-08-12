2c89a59af9924b042dbf302b5ed491e1c88f4b1b
tsweetheart <juan-daniel1914@hotmail.com>
2021-08-12 10:05:45 -0500

    part 2

--- js/test.js
+++
@@ -1,8 +0,0 @@
-let startTest = document.getElementsByClassName("button__startest")[0];
-startTest.addEventListener("click", () => {
-	if (logged === false) {
-		printMessage("#FFE1DE", "#F47174", `Login to do test`, "error");
-	} else {
-		window.location.href = "./testquestions.html";
-	}
-});
