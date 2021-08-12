c68f1381a95a0b1a11e4ac1ca53309e51b85a72d
tsweetheart <juan-daniel1914@hotmail.com>
2021-08-12 09:42:08 -0500

    match part 1

--- js/footer.js
+++ js/footer.js
@@ -5,48 +5,55 @@ footerTemplate.innerHTML = `
         <div class="img-background-footer">
             <div class="mobile content-logo-footer ">
                 <div class="logo-footer">
-                    <img src="../assets/footer/logo.png" alt="Logo">
+                    <img src="../assets/footer/logo.png" alt="">
                 </div>
             </div>
-            <div class="background-menu mobile">
-                <div class="menu-footer-mobile">
+            <div class="background-menu mobile" style="display:flex;justify-content: space-around;align-items: center;">
+                <div class="menu-footer-mobile mobile" style="width:50%;">
                     <ul>
+                        <li> <a href="../index.html"> Home</a></li>
+                        <li> <a href="./experts.html"> Services</a></li>
                         <li> <a href="./learn.html"> Learn more</a></li>
-                        <li><a href="./contact.html"> Contact</a></li>
                         <li><a href="./aboutUs.html"> About us</a></li>
                     </ul>
                 </div>
+                <div class="networks mobile" style="width:50%;">
+                <div class="img-networks">
+                <a href="#" target="blank"><i class="fab fa-facebook"></i></a>
+                <a href="#" target="blank"><i class="fab fa-twitter"></i></i></a>
+                <a href="#" target="blank"><i class="fab fa-instagram"></i></i></a>
+                <a href="#" target="blank"><i class="fab fa-linkedin"></i></a>
+                </div>
+            </div>
 
             </div>
             <div class="background-menu desktop">
                 <div class="menu-footer">
                     <ul>
-                    <li> <a href="./learn.html"> Learn more</a></li>
-                    <li><a href="./contact.html"> Contact</a></li>
-                    <li><a href="./aboutUs.html"> About us</a></li>
+                    <li> <a href="../index.html"> Home</a></li>
+                        <li> <a href="./experts.html"> Services</a></li>
+                        <li> <a href="./learn.html"> Learn more</a></li>
+                        <li><a href="./aboutUs.html"> About us</a></li>
                     </ul>
                 </div>
                 <div class="content-logo-footer">
                     <div class="logo-footer">
-                        <img src="../assets/footer/logo.png" alt="">
+                        <a href="../index.html">
+                            <img src="../assets/footer/logo.png" alt="relyer logo">
+                        </a>
                     </div>
                 </div>
                 <div class="networks">
                     <div class="img-networks">
-                        <img src="../assets/footer/markunread.png" alt="Correo">
-                        <img src="../assets/footer/Instagram.png" alt="Instagram">
-                        <img src="../assets/footer/Facebook.png" alt="Facebook">
+                        <a href="#" target="blank"><i class="fab fa-facebook"></i></a>
+                        <a href="#" target="blank"><i class="fab fa-twitter"></i></i></a>
+                        <a href="#" target="blank"><i class="fab fa-instagram"></i></i></a>
+                        <a href="#" target="blank"><i class="fab fa-linkedin"></i></a>
                     </div>
                 </div>
 
             </div>
-            <div class="networks mobile">
-                <div class="img-networks">
-                    <img src="../assets/footer/markunread.png" alt="Correo">
-                    <img src="../assets/footer/Instagram.png" alt="Instagram">
-                    <img src="../assets/footer/Facebook.png" alt="Facebook">
-                </div>
-            </div>
+        
         </div>
 `;
 
